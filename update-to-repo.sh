c3532b1f82d42913dac9f7f8416d53886277ca71
Thang Vo <thangvo8797@gmail.com>
2022-02-18 10:25:12 +0700

    Update utils file

--- update-to-repo.sh
+++ update-to-repo.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 echo "Updating vimrc from machine"
-cp ~/vimrc .
+cp ~/.vimrc ./vimrc
 
 echo "Updating tmux conf from machine"
-cp ~/tmux.conf .
+cp ~/.tmux.conf ./tmux.conf
 
 echo "Updating alacritty from machine"
 cp ~/.config/alacritty.yml .
 
-if [ "$(uname)" <> "Darwin" ]; then
+if [ "$(uname)" = "Linux" ]; then
   echo "Updating sxhkdrc"
-  sudo $HOME/.config/sxhkd ./sxhkdrc
+  sudo cp $HOME/.config/sxhkd ./sxhkdrc
 fi
 
 git add vimrc tmux.conf alacritty.yml
